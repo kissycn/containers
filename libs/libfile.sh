8cfc783c4660779eb15cbfdf3e7dbcf688c79517
kissy <kissycn@gmail.com>
2023-10-18 10:26:04 +0800

    feat: add basic libs Basic tools and methods are encapsulated based on Bitnami scripts, including: logs, net, file operations, user, usergroup, type verification, etc.

---
+++ libs/libfile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright VMware, Inc.
+# SPDX-License-Identifier: APACHE-2.0
+#
+# Library for managing files
+
+# shellcheck disable=SC1091
+
+# Load Generic Libraries
+. /opt/scripts/libs/libos.sh
+
+# Functions
+
+########################
+# Replace a regex-matching string in a file
+# Arguments:
+#   $1 - filename
+#   $2 - match regex
+#   $3 - substitute regex
+#   $4 - use POSIX regex. Default: true
+# Returns:
+#   None
+#########################
+replace_in_file() {
+    local filename="${1:?filename is required}"
+    local match_regex="${2:?match regex is required}"
+    local substitute_regex="${3:?substitute regex is required}"
+    local posix_regex=${4:-true}
+
+    local result
+
+    # We should avoid using 'sed in-place' substitutions
+    # 1) They are not compatible with files mounted from ConfigMap(s)
+    # 2) We found incompatibility issues with Debian10 and "in-place" substitutions
+    local -r del=$'\001' # Use a non-printable character as a 'sed' delimiter to avoid issues
+    if [[ $posix_regex = true ]]; then
+        result="$(sed -E "s${del}${match_regex}${del}${substitute_regex}${del}g" "$filename")"
+    else
+        result="$(sed "s${del}${match_regex}${del}${substitute_regex}${del}g" "$filename")"
+    fi
+    echo "$result" > "$filename"
+}
