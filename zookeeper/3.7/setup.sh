c3a4298fa70d005497f0a78d9c054080f1b6229f
kissycn <kissycn@gmail.com>
2023-10-18 15:55:07 +0800

    fix: udpate import shell files paths

--- zookeeper/3.7/setup.sh
+++ zookeeper/3.7/setup.sh
@@ -8,18 +8,17 @@ set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purposes
 
 # Load libraries
-. /opt/libs/libfs.sh
-. /opt/libs/libos.sh
+. /opt/scripts/libs/libfs.sh
+. /opt/scripts/libs/libos.sh
 
 # Load ZooKeeper environment variables
-. /opt/zookeeper/3.7/env.sh
+. /opt/scripts/zookeeper/3.7/env.sh
 
 ensure_user_exists "$ZOO_DAEMON_USER" --uid 10000 --group "$ZOO_DAEMON_GROUP"
 
 # Ensure directories used by ZooKeeper exist and have proper ownership and permissions
 for dir in "$ZOO_DATA_DIR" "$ZOO_CONF_DIR" "$ZOO_LOG_DIR"; do
     ensure_dir_exists "$dir"
-    # chmod -R 777 "$dir"
 done
 
 chown -R "$ZOO_DAEMON_USER":"$ZOO_DAEMON_GROUP" $ZOO_BASE_DIR
